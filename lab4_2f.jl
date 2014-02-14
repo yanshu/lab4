734ad6001e5fe6d4c5d7839ddadf8e4541d607c8
yanshu <huangfeifei91@gmail.com>
2014-02-14 07:46:14 -0500

    update

--- lab4_2f.jl
+++ lab4_2f.jl
@@ -1,12 +1,13 @@
 Pkg.add("Devectorize")
-
+using Devectorize
 function integral(a::Float64,b::Float64,N::Int64)
         x = Array(Float64,N);
         dx = (b-a)/(N+1);
         for i = 1:N
                 x[i] = a + dx*i
         end
-        @devec f = dx.*exp(-0.5.*x.^2)/sqrt(2.*pi);
+        c = 1/sqrt(2*pi);
+        @devec f = c.*dx.*exp(-0.5.*x.^2);
         return sum(f);
 end
 
